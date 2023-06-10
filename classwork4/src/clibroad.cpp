fcad4b4d3c69c9f760a919507141ec1ad308d632
vincenzoiorio <127449936+vincenzoiorio@users.noreply.github.com>
2023-06-10 17:49:12 +0200

    Add files via upload

--- classwork4/src/clibroad.cpp
+++ classwork4/src/clibroad.cpp
@@ -1,3 +1,4 @@
+/*
 #include "ros/ros.h"
 #include "classwork4/fileservice.h"
 #include <geometry_msgs/Pose.h>
@@ -6,14 +7,15 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+
     ros::init(argc, argv, "client_broadcaster");
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<classwork4::fileservice>("trservice");
     
     classwork4::fileservice srv;
-
-    srv.request.frame_a.data="/lbr_iiwa_link_0";
-    srv.request.frame_b.data="/lbr_iiwa_link_7";
+    
+    srv.request.frame_a.data="/lbr_iiwa_link_7";
+    srv.request.frame_b.data="/lbr_iiwa_link_0";
 
 
     ROS_INFO("Waiting for the client server");
@@ -25,17 +27,91 @@ int main(int argc, char** argv) {
 		ROS_ERROR("Error calling the service");
 		return 1;
 	}
-    /* while(ros::ok()){
+    
+    tf::TransformBroadcaster br;
+    tf::Transform transform;
+    ros::Rate r(100);
 
-        }
-        */
+
+    while(ros::ok()){
+
+    tf::Quaternion q;
+    q.setRPY(0, 0, 0);  // Angoli di rollio, beccheggio e imbardata
+    q.normalize();
+    transform.setRotation(q);
+    
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(),srv.request.frame_a.data ,srv.request.frame_b.data ));
+   
+    r.sleep();
+    }
+    
        
+
+
+
+	//Just print the output    
+    cout << "Service output: " <<endl<<srv.response.pose << endl;
+    return 0;
+}
+*/
+#include "ros/ros.h"
+#include "classwork4/fileservice.h"
+#include <geometry_msgs/Pose.h>
+#include <tf/transform_broadcaster.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "client_broadcaster");
+    ros::NodeHandle n;
+    ros::ServiceClient client = n.serviceClient<classwork4::fileservice>("trservice");
+    string frame_a;
+    string frame_b;
+    
+    tf::Transform transform;
+    tf::TransformBroadcaster br;
+    tf::Quaternion q;
+
+    classwork4::fileservice srv;
+    cout << "Choose the frame a"<<endl;
+    cin>>frame_a;
+    cout << "Choose the frame b"<<endl;
+    cin>>frame_b;
+   
+
+
+    ROS_INFO("Waiting for the client server");
+	client.waitForExistence(); 
+	ROS_INFO("Client server up now");
+    ros::Rate r(100);
+while(ros::ok()){
+
+    if (!client.call(srv)) {
+		ROS_ERROR("Error calling the service");
+		return 1;
+	}
+     srv.request.frame_a.data=frame_a;
+     srv.request.frame_b.data=frame_b;
 	//Just print the output
 	cout << "Service output: " << srv.response.pose << endl;
 
+    
+        // Set the translation of the transform
+        transform.setOrigin(tf::Vector3(
+            srv.response.pose.position.x,
+            srv.response.pose.position.y,
+            srv.response.pose.position.z
+        ));
+        
+        // Set the rotation of the transform
+        q.setRPY(srv.response.pose.orientation.x,srv.response.pose.orientation.y,srv.response.pose.orientation.z);
+        transform.setRotation(q);
+
+        // Broadcast the transform between frame_a and frame_b
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_a, frame_b));
 
 
-    // ros::Rate r(50);
-  
+     r.sleep();
+}
     return 0;
 }
--- classwork4/src/servelist.cpp
+++ classwork4/src/servelist.cpp
@@ -1,3 +1,4 @@
+/*
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
 #include <classwork4/fileservice.h>
@@ -10,7 +11,7 @@ bool service_callback( classwork4::fileservice::Request &req, classwork4::filese
   cout << "Service frame received: " << endl;
   //We know that the service is called with 2 parameters: frame_a and frame_b
 	// These parameters are put in a data structure called req
-	cout << req.frame_a.data << req.frame_b.data << endl;
+	cout << req.frame_a.data <<endl<< req.frame_b.data << endl;
 
 	//The return value is store in the res datastracture
 	cout << "Returning the frame_b in the frame_a: " << endl;
@@ -20,8 +21,8 @@ bool service_callback( classwork4::fileservice::Request &req, classwork4::filese
 
  tf::StampedTransform transform;
         try {
-            //listener.lookupTransform(req.frame_b.data, req.frame_a.data, ros::Time(0), transform); //se mettere nome
-            listener.lookupTransform("/lbr_iiwa_link_7", "/lbr_iiwa_link_0", ros::Time(0), transform);
+            listener.waitForTransform(req.frame_a.data, req.frame_b.data, ros::Time(0), ros::Duration(3.0));
+            listener.lookupTransform(req.frame_a.data, req.frame_b.data, ros::Time(0), transform);
         }
         catch (tf::TransformException ex){
             ROS_ERROR("%s",ex.what());
@@ -37,12 +38,120 @@ bool service_callback( classwork4::fileservice::Request &req, classwork4::filese
             transform.getRotation().y() << ", " << 
             transform.getRotation().z()
         );
-
-
+      res.pose.position.x=transform.getOrigin().x();
+      res.pose.position.y=transform.getOrigin().y();
+      res.pose.position.z=transform.getOrigin().z();
+      res.pose.orientation.x=transform.getRotation().x();
+      res.pose.orientation.y=transform.getRotation().y();
+      res.pose.orientation.z=transform.getRotation().z();
 
 
 	return true;
 
+}
+
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "server_listener");
+
+    ros::NodeHandle nh;
+    ros::ServiceServer service = nh.advertiseService("trservice", service_callback);
+    
+    ros::spin();
+
+    return 0;
+}
+*/
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+#include <classwork4/fileservice.h>
+#include <tf/transform_listener.h>
+
+using namespace std;
+
+// bool service_callback( classwork4::fileservice::Request &req, classwork4::fileservice::Response &res) {
+//   tf::TransformListener listener;
+//   cout << "Service frame received: " << endl;
+//   //We know that the service is called with 2 parameters: frame_a and frame_b
+// 	// These parameters are put in a data structure called req
+// 	cout << req.frame_a.data << req.frame_b.data << endl;
+
+// 	//The return value is store in the res datastracture
+// 	cout << "Returning the frame_b in the frame_a: " << endl;
+	
+//   //res.sum.data = req.x.data + req.y.data;
+//   //res.pose
+
+//  tf::StampedTransform transform;
+//         try {
+//             listener.waitForTransform("/lbr_iiwa_link_0","/lbr_iiwa_link_7", ros::Time(0), ros::Duration(3.0));
+//             //listener.lookupTransform(req.frame_b.data, req.frame_a.data, ros::Time(0), transform); //se mettere nome
+//             listener.lookupTransform("/lbr_iiwa_link_0","/lbr_iiwa_link_7", ros::Time(0), transform);
+//         }
+//         catch (tf::TransformException ex){
+//             ROS_ERROR("%s",ex.what());
+//             ros::Duration(1.0).sleep();
+//         }
+
+//         ROS_INFO_STREAM(" Transform:" << 
+        
+//             transform.getOrigin().x() << ", " << 
+//             transform.getOrigin().y() << ", " <<
+//             transform.getOrigin().z() << ", " << 
+//             transform.getRotation().x() << ", " << 
+//             transform.getRotation().y() << ", " << 
+//             transform.getRotation().z()
+//         );
+
+
+
+
+// 	return true;
+
+// }
+bool service_callback(classwork4::fileservice::Request &req, classwork4::fileservice::Response &res) {
+    tf::TransformListener listener;
+    cout << "Service frame received: " << endl;
+    cout << req.frame_a.data << req.frame_b.data << endl;
+
+    cout << "Returning the frame_b in the frame_a: " << endl;
+
+    tf::StampedTransform transform;
+    try {
+        listener.waitForTransform(req.frame_a.data, req.frame_b.data, ros::Time(0), ros::Duration(1.0));
+        listener.lookupTransform(req.frame_a.data, req.frame_b.data, ros::Time(0), transform);
+    } catch (tf::TransformException ex) {
+        ROS_ERROR("%s", ex.what());
+        ros::Duration(1.0).sleep();
+    }
+
+    ROS_INFO_STREAM(" Transform:" <<
+        transform.getOrigin().x() << ", " <<
+        transform.getOrigin().y() << ", " <<
+        transform.getOrigin().z() << ", "
+    );
+
+    tf::Quaternion quat = transform.getRotation();
+    tf::Matrix3x3 matrix(quat);
+    double roll, pitch, yaw;
+    matrix.getRPY(roll, pitch, yaw);
+
+    ROS_INFO_STREAM(" Rotation (RPY):" <<
+        roll << ", " <<
+        pitch << ", " <<
+        yaw
+    );
+    res.pose.position.x = transform.getOrigin().x();
+    res.pose.position.y = transform.getOrigin().y();
+    res.pose.position.z = transform.getOrigin().z();
+    res.pose.orientation.x = roll;
+    res.pose.orientation.y = pitch;
+    res.pose.orientation.z = yaw;
+
+
+
+    return true;
 }
 
 
